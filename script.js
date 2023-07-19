5e873aa6d4c11e6b10de3679f494ac5adcf40fa3
♥ Mass ♥ <58617118+heyitsmass@users.noreply.github.com>
2023-07-19 07:54:05 -0700

    Converted to Under Construction

---
+++ script.js
@@ -0,0 +1,22 @@
+// Set the date we're counting down to
+var countDownDate = new Date("2023-12-31T00:00:00Z").getTime();
+
+var countdownfunction = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (distance < 0) {
+    clearInterval(countdownfunction);
+    document.getElementById("countdown").innerHTML = "WE ARE LIVE!";
+  }
+}, 1000);
